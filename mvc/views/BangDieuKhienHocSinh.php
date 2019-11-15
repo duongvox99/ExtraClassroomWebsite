300377862dc7ac724d0d49d4f58e12a0930abfbe
Jason-Pham-1100011 <54681042+Jason-Pham-1100011@users.noreply.github.com>
2019-11-15 22:04:37 +0700

    Create Head Folder

--- mvc/views/BangDieuKhienHocSinh.php
+++ mvc/views/BangDieuKhienHocSinh.php
@@ -2,21 +2,7 @@
 <html lang="vi">
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Project</title>
-
-	<link rel="stylesheet" type="text/css" href="/ExtraClassroomWebsite/public/css/bootstrap.css" />
-	<link rel="stylesheet" type="text/css" href="/ExtraClassroomWebsite/public/css/LamBai.css" />
-	
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-	<script type='text/javascript' src='/ExtraClassroomWebsite/public/js/jquery-3.4.1.js'></script>
-	<script type='text/javascript' src='/ExtraClassroomWebsite/public/js/LamBai.js'></script>
-	<script id="MathJax-script" async src="/ExtraClassroomWebsite/public/js/tex-mml-chtml.js"></script>
+	<?php require("heads/BaiLam.php") ?>
 </head>
 
 <body >
