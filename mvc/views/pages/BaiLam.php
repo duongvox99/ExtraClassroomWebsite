d31fed3b02dc62e40522ddd0415a24469660e32e
Jason-Pham-1100011 <54681042+Jason-Pham-1100011@users.noreply.github.com>
2019-11-15 21:59:20 +0700

    BaiLam

---
+++ mvc/views/pages/BaiLam.php
@@ -0,0 +1,59 @@
+<div class="container border border-dark bg-light">
+		<div class="row">
+			<div class="col col-lg-8 col-md-12 col-sm-12 col-12">
+				<div class="card border-0 shadow my-5 new-banner position-relative">
+					<div class="position-absolute new-banner-location">
+						<img class="new-banner-svg" alt="New" src="https://hrcdn.net/fcore/assets/svgs/new-banner-341e41b06d.svg">
+					</div>
+					<div class="card-body p-5">
+						<h1 class="font-weight-light"><b>Bài kiểm tra 1</b></h1>
+						<p class="lead">Ngày xxx thi phòng yyy Vào lúc zzz Thời gian xxx</p>
+						<p class="lead">Vui lòng có mặt đúng giờ...</p>
+						<p class="lead mb-0">Mọi thắc mắc và phản hồi vui lòng liên hệ ???</p>
+					</div>
+				</div>
+				<div class="card border-0 shadow my-5">
+					<div class="card-body p-5">
+						<h1 class="font-weight-light"><b>Bài kiểm tra 2</b></h1>
+						<p class="lead">Ngày xxx thi phòng yyy Vào lúc zzz Thời gian xxx</p>
+						<p class="lead">Vui lòng có mặt đúng giờ...</p>
+						<p class="lead mb-0">Mọi thắc mắc và phản hồi vui lòng liên hệ ???</p>
+					</div>
+				</div>
+				<div class="card border-0 shadow my-5">
+					<div class="card-body p-5">
+						<h1 class="font-weight-light"><b>Bài kiểm tra 3</b></h1>
+						<p class="lead">Ngày xxx thi phòng yyy Vào lúc zzz Thời gian xxx</p>
+						<p class="lead">Vui lòng có mặt đúng giờ...</p>
+						<p class="lead mb-0">Mọi thắc mắc và phản hồi vui lòng liên hệ ???</p>
+					</div>
+				</div>
+				<div class="card border-0 shadow my-5">
+					<div class="card-body p-5">
+						<h1 class="font-weight-light"><b>Bài kiểm tra 4</b></h1>
+						<p class="lead">Ngày xxx thi phòng yyy Vào lúc zzz Thời gian xxx</p>
+						<p class="lead">Vui lòng có mặt đúng giờ...</p>
+						<p class="lead mb-0">Mọi thắc mắc và phản hồi vui lòng liên hệ ???</p>
+					</div>
+				</div>		
+			</div>
+			<div class="col col-lg-4 col-md-12 col-sm-12 col-12 ">
+					<div class="card border-0 shadow ml-auto my-5 custom-collapse">
+						<div class="card-header bg-success" id="headingOne" data-parent="custom-collapse">
+							<h5 class="mb-0">
+								<a class="collapse-toggle visible-xs" data-toggle="collapse" aria-expanded="true" aria-controls="side-menu-collapse" data-target="#side-menu-collapse">
+									Loại Bài kiểm tra
+								</a>
+							</h5>
+						</div>
+						<div class="card-body">
+							<div class="list-group cusstom-listgroup" role="tablist" class="list-group collapse" id="side-menu-collapse">
+								<a class="list-group-item list-group-item-action" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="">Tất cả</a>
+								<a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="">Chưa xem</a>
+								<a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="">Đã xem</a>
+							</div>
+						</div>
+					</div>
+			</div>
+		</div>
+	</div>
