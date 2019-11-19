f125410302d8fab357e9edb69cdd9d6975815b73
Duong Vo <duongvox99@gmail.com>
2019-11-19 18:49:17 +0700

    Update LamBai.php

--- mvc/views/heads/LamBai.php
+++ mvc/views/heads/LamBai.php
@@ -7,16 +7,11 @@
 	<link rel="stylesheet" type="text/css" href="/ExtraClassroomWebsite/public/css/LamBai.css" />
 	<link rel="stylesheet" type="text/css" href="/ExtraClassroomWebsite/public/font-awesome/css/all.css" />
 	
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
 	<script type='text/javascript' src='/ExtraClassroomWebsite/public/js/jquery-3.4.1.js'></script>
 	<script type='text/javascript' src='/ExtraClassroomWebsite/public/js/LamBai.js'></script>
 	<script id="MathJax-script" async src="/ExtraClassroomWebsite/public/js/tex-mml-chtml.js"></script>
 
-
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Bảng điều khiển học sinh</title>
+	<title>Làm bài kiểm tra | Bảng điều khiển học sinh</title>
