7bff2674b9b76e9c7d9832af8bc4699f271d9375
Duong Vo <duongvox99@gmail.com>
2019-11-24 18:13:58 +0700

    KenhThaoLuanChung

--- mvc/views/pages/KenhThaoLuanChung.php
+++ mvc/views/pages/KenhThaoLuanChung.php
@@ -1,6 +1,17 @@
 <script type="text/javascript" src="/ExtraClassroomWebsite/public/js/ckeditor.js"></script>
 <script id="MathJax-script" async src="/ExtraClassroomWebsite/public/js/tex-mml-chtml.js"></script>
-<div class="<?php echo (isset($data["GiaoVien"])) ? "container-fluid" : "container";?> bg-light">
+
+<?php
+echo "<script> var idUser = " . $data["DataNguoiDung"]["IdNguoiDung"] . "; </script>";
+echo "<script> var nameUser = '" . $data["DataNguoiDung"]["HoTen"] . "'; </script>";
+if ($data["DataNguoiDung"]["Avatar"] != "") {
+	echo "<script> var avatarUser = '" . $data["DataNguoiDung"]["Avatar"] . "'; </script>";
+} else {
+	echo "<script> var avatarUser = '/ExtraClassroomWebsite/public/img/no-avatar.png'; </script>";
+}
+?>
+
+<div class="<?php echo (isset($data["GiaoVien"])) ? "container-fluid" : "container"; ?> bg-light">
 	<div class="h-100">
 		<div class="row justify-content-center h-100">
 			<div class="col-md-12 col-xl-12 chat">
@@ -17,49 +28,14 @@
 							</div>
 						</div>
 					</div>
-					<div class="card-body msg_card_body">
-						<div class="d-flex justify-content-start mb-4">
-							<div class="img_cont_msg">
-								<img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg">
-								<p class="name">V.V.Dương</p>
-							</div>
-							<div class="msg_cotainer">
-								Hi, how are you samim?
-								<span class="msg_time">8:40 AM, Today</span>
-							</div>
-						</div>
-						<div class="d-flex justify-content-end mb-4">
-							<div class="msg_cotainer_send">
-								Hi Khalid i am good tnx how about you?
-								<span class="msg_time_send">8:55 AM, Today</span>
-							</div>
-							<div class="img_cont_msg">
-								<img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg">
-							</div>
-						</div>
 
-						<div class="d-flex justify-content-start mb-4">
-							<div class="img_cont_msg">
-								<img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg">
-							</div>
-							<div class="msg_cotainer">
-								I am looking for your next templates
-								<span class="msg_time">9:07 AM, Today</span>
-							</div>
-						</div>
-						<div class="d-flex justify-content-start mb-4">
-							<div class="img_cont_msg">
-								<img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img_msg">
-							</div>
-							<div class="msg_cotainer">
-								Bye, see you
-								<span class="msg_time">9:12 AM, Today</span>
-							</div>
-						</div>
+					<div class="card-body msg_card_body" id="messageWrapper">
+
 					</div>
+
 					<div class="card-footer">
-						<textarea name="InputMessage" id="editorInputMessage"></textarea>
-						<button class="btn btn-success ml-auto btn-block">Gửi tin nhắn <i class="fas fa-location-arrow"></i></button>
+						<textarea id="editorInputMessage"></textarea>
+						<button class="btn btn-success ml-auto btn-block" onclick="pushMessage()">Gửi tin nhắn <i class="fas fa-location-arrow"></i></button>
 					</div>
 				</div>
 			</div>
@@ -69,7 +45,133 @@
 <script>
 	ClassicEditor
 		.create(document.querySelector('#editorInputMessage'))
+		.then(editor => {
+			inputMessage = editor;
+		})
 		.catch(error => {
 			console.error(error);
 		});
+</script>
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.5.0/firebase-app.js"></script>
+
+<script src="https://www.gstatic.com/firebasejs/7.5.0/firebase-firestore.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.5.0/firebase-analytics.js"></script>
+
+<script>
+	// Your web app's Firebase configuration
+	var firebaseConfig = {
+		apiKey: "AIzaSyDXfzYiJDArkPeZ4m5184VuD3GzFQBK9DQ",
+		authDomain: "extraclassroomwebsite.firebaseapp.com",
+		databaseURL: "https://extraclassroomwebsite.firebaseio.com",
+		projectId: "extraclassroomwebsite",
+		storageBucket: "extraclassroomwebsite.appspot.com",
+		messagingSenderId: "143656346318",
+		appId: "1:143656346318:web:68e946be657f5e6ceea69f",
+		measurementId: "G-QKJJNL1VBJ"
+	};
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
+	firebase.analytics();
+
+	console.log(idUser, nameUser);
+
+	var oldData = [];
+
+	var db = firebase.firestore();
+
+	function pushMessage() {
+		let message = inputMessage.getData();
+
+		if (message != "") {
+			let today = new Date();
+			let date = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
+			let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+			let currentDateTime = time + ' ' + date;
+
+			db.collection("kenhthaoluanchung").add({
+					currentDateTime: currentDateTime,
+					idUser: idUser,
+					nameUser: nameUser,
+					avatarUser: avatarUser,
+					message: message,
+					sort: firebase.firestore.FieldValue.serverTimestamp(),
+				})
+				.then(function(docRef) {
+					inputMessage.setData("");
+					return true;
+				})
+				.catch(function(error) {
+					console.error("Error pushMessage: ", error);
+					return false;
+				});
+		} else {
+			return false;
+		}
+	}
+
+	setInterval(() => {
+		db.collection("kenhthaoluanchung").orderBy("sort").get()
+			.then((querySnapshot) => {
+				let tmp = [];
+				querySnapshot.forEach((doc) => {
+					tmp.push(doc.data());
+				});
+				// console.log(JSON.stringify(oldData));
+				// console.log(JSON.stringify(tmp));
+
+				if (JSON.stringify(oldData) != JSON.stringify(tmp)) {
+					document.getElementById("messageWrapper").innerHTML = "";
+					oldData = tmp;
+
+					querySnapshot.forEach((doc) => {
+						$data = doc.data();
+
+						// console.log($data);
+
+						if ($data["idUser"] == idUser) {
+							let htmlCurrentUserMessage =
+								'<div class="d-flex justify-content-end mb-4">\n' +
+								'<div class="msg_cotainer_send">\n' +
+								$data["message"] + '\n' +
+								'<span class="msg_time_send">' + $data["currentDateTime"] + '</span>\n' +
+								'</div>\n' +
+								'<div class="img_cont_msg">\n' +
+								'<img src="' + $data["avatarUser"] + '" class="rounded-circle user_img_msg">\n' +
+								'<p class="name">' + $data["nameUser"] + '</p>\n' +
+								'</div>\n' +
+								'</div>';
+
+							document.getElementById("messageWrapper").innerHTML += htmlCurrentUserMessage;
+						} else {
+							let htmlOtherUserMessage =
+								'<div class="d-flex justify-content-start mb-4">\n' +
+								'<div class="img_cont_msg">\n' +
+								'<img src="' + $data["avatarUser"] + '" class="rounded-circle user_img_msg">\n' +
+								'<p class="name">' + $data["nameUser"] + '</p>\n' +
+								'</div>\n' +
+								'<div class="msg_cotainer">\n' +
+								$data["message"] + '\n' +
+								'<span class="msg_time">' + $data["currentDateTime"] + '</span>\n' +
+								'</div>\n' +
+								'</div>';
+
+							document.getElementById("messageWrapper").innerHTML += htmlOtherUserMessage;
+						}
+					});
+
+					var objDiv = document.getElementById("messageWrapper");
+					objDiv.scrollTop = objDiv.scrollHeight;
+
+				}
+			})
+			.catch(function(error) {
+				console.error("Error refreshMessages: ", error);
+				return false;
+			});
+	}, 1000);
 </script>
